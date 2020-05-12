621daab2651b69cad535f967c8d339c5e676bfd4
Sergei Medvedev <cepaleme@gmail.com>
2020-05-12 18:50:40 +0300

    dev : починили глубину вложенности параметров в категории

--- yavi-visual/category.cpp
+++ yavi-visual/category.cpp
@@ -100,7 +100,7 @@ void  TCategory::getCategories( const YAML::Node&  node, TParam  *a_pParam, int 
         for( auto& par : node[ GoodsParametersSection ] )
         {
             TParam  *pParam;
-            pParam = new TParam( pCategories->getCategoriesDepth() );
+            pParam = new TParam( pCategories->getCategoriesDepth() + 1 );
 
             // добавляем Parameters в список класса TCategories
             pCategories->m_apParamList.append(pParam);
